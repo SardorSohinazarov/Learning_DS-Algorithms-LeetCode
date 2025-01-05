c8986f0152281039a87789f74e5bac1f8f593c87
SardorSohinazarov <sardorstudent0618@gmail.com>
2025-01-05 21:16:18 +0500

    Yaxshi ekan

---
+++ LeetCode/Medium/189. Rotate Array.cs
@@ -0,0 +1,30 @@
+﻿
+namespace LeetCode.Medium._189
+{
+    internal class _189
+    {
+    }
+
+    class Solution
+    {
+        public int[] rotate(int[] nums, int k)
+        {
+            k = k % nums.Length;
+            Reverse(nums, 0, nums.Length - 1);
+            Reverse(nums, 0, k - 1);
+            Reverse(nums, k, nums.Length - 1);
+
+            return nums;
+        }
+
+        private void Reverse(int[] nums, int start, int end)
+        {
+            for (int i = start, j = end; i < j; i++, j--)
+            {
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+            }
+        }
+    }
+}
