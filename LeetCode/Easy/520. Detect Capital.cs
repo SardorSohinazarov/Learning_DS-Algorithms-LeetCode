0b239bc0d34bb5724fc69561b2f73818f62cef0c
Sohinazarov Sardorbek O'rinboy o'gli <Sohinazarov_S@dt.local>
2025-10-31 16:29:44 +0500

    520

---
+++ LeetCode/Easy/520. Detect Capital.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.RegularExpressions;
+
+namespace LeetCode.Easy._520;
+
+public class _520
+{
+    public class Solution
+    {
+        //public static void Main()
+        //{
+        //    string word = "g";
+        //    Console.WriteLine(DetectCapitalUse(word));
+        //}
+
+        public static bool DetectCapitalUse(string word)
+        {
+            var allUpper = "^[A-Z]+$";
+            var allLower = "^[a-z]+$";
+            var firstCapital = "^[A-Z][a-z]*$";
+
+            return Regex.IsMatch(word, firstCapital) || Regex.IsMatch(word, allLower) || Regex.IsMatch(word, allUpper);
+        }
+    }
+}
