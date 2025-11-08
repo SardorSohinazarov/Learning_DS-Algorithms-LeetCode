b4ff2de5ce79333502f03f9676442f8d88aba92b
Sohinazarov Sardorbek O'rinboy o'gli <Sohinazarov_S@dt.local>
2025-11-08 17:17:36 +0500

    5

---
+++ LeetCode/Medium/5. Longest Palindromic Substring.cs
@@ -0,0 +1,44 @@
+﻿namespace LeetCode.Medium
+{
+    internal class _5
+    {
+        public class Solution
+        {
+            public string LongestPalindrome(string s)
+            {
+                if (s.Length <= 1)
+                    return s;
+
+                string longest = s[0].ToString();
+
+                for (int i = 0; i < s.Length; i++)
+                {
+                    var rightIndex = s.Length - 1;
+                    while (rightIndex > i)
+                    {
+                        if (IsPalindrome(s, i, rightIndex))
+                        {
+                            if (longest.Length < rightIndex - i + 1)
+                                longest = s.Substring(i, rightIndex - i + 1);
+                        }
+                        rightIndex--;
+                    }
+                }
+
+                return longest;
+            }
+
+            public bool IsPalindrome(string s, int left, int right)
+            {
+                while (left < right)
+                {
+                    if (s[left] != s[right])
+                        return false;
+                    left++;
+                    right--;
+                }
+                return true;
+            }
+        }
+    }
+}
