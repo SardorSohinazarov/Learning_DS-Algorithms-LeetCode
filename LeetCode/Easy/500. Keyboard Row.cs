b8e1c7a71b1113d8f34fc547e6206948763aa94d
Sohinazarov Sardorbek O'rinboy o'gli <Sohinazarov_S@dt.local>
2025-11-09 15:11:26 +0500

    500

---
+++ LeetCode/Easy/500. Keyboard Row.cs
@@ -0,0 +1,35 @@
+﻿namespace LeetCode.Easy
+{
+    internal class _500
+    {
+        public class Solution
+        {
+            public string[] FindWords(string[] words)
+            {
+                var firstRow = new HashSet<char>("qwertyuiopQWERTYUIOP");
+                var secondRow = new HashSet<char>("asdfghjklASDFGHJKL");
+                var thirdRow = new HashSet<char>("zxcvbnmZXCVBNM");
+
+                var result = new List<string>();
+
+                foreach (var ord in words)
+                {
+                    if (IsInRow(ord, firstRow) || IsInRow(ord, secondRow) || IsInRow(ord, thirdRow))
+                        result.Add(ord);
+                }
+
+                return result.ToArray();
+            }
+
+            bool IsInRow(string word, HashSet<char> row)
+            {
+                foreach (var ch in word)
+                {
+                    if (!row.Contains(ch))
+                        return false;
+                }
+                return true;
+            }
+        }
+    }
+}
