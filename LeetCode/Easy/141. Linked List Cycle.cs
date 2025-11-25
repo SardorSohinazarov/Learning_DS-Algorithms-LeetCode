64cee4c32ea1766138d4a4deed59be46dade7e30
Sohinazarov Sardorbek O'rinboy o'gli <Sohinazarov_S@dt.local>
2025-11-25 15:29:13 +0500

    141, 2390

---
+++ LeetCode/Easy/141. Linked List Cycle.cs
@@ -0,0 +1,36 @@
+﻿namespace LeetCode.Easy
+{
+    internal class _141
+    {
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+            public ListNode(int x)
+            {
+                val = x;
+                next = null;
+            }
+        }
+
+        public class Solution
+        {
+            public bool HasCycle(ListNode head)
+            {
+                var slow = head;
+                var fast = head;
+
+                while (fast != null && fast.next != null)
+                {
+                    slow = slow.next;
+                    fast = fast.next.next;
+                    if (slow == fast)
+                    {
+                        return true;
+                    }
+                }
+                return false;
+            }
+        }
+    }
+}
---
+++ LeetCode/Medium/2390. Removing Stars From a String.cs
@@ -0,0 +1,30 @@
+﻿namespace LeetCode.Medium
+{
+    internal class _2390
+    {
+        public class Solution
+        {
+            public string RemoveStars(string s)
+            {
+                Stack<char> stack = new Stack<char>();
+
+                foreach (char c in s)
+                {
+                    if (c == '*')
+                    {
+                        if (stack.Count > 0)
+                        {
+                            stack.Pop();
+                        }
+                    }
+                    else
+                    {
+                        stack.Push(c);
+                    }
+                }
+
+                return string.Join("" ,stack.ToArray().Reverse());
+            }
+        }
+    }
+}
