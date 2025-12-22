f2750d6df546e7b12c0ff3ecd5781a405340dc82
Sohinazarov Sardorbek O'rinboy o'gli <Sohinazarov_S@dt.local>
2025-12-22 18:04:33 +0500

    547

---
+++ LeetCode/Medium/0547_NumberofProvinces.cs
@@ -0,0 +1,38 @@
+﻿namespace LeetCode.Medium
+{
+    internal class _0547_NumberofProvinces
+    {
+        public class Solution
+        {
+            public int FindCircleNum(int[][] isConnected)
+            {
+                int n = isConnected.Length;
+                bool[] visited = new bool[n];
+                int count = 0;
+                for (int i = 0; i < n; i++)
+                {
+                    if (!visited[i])
+                    {
+                        count++;
+                        DFS(isConnected, visited, i);
+                    }
+                }
+
+                return count;
+            }
+
+            private void DFS(int[][] isConnected,bool[] visited, int node)
+            {
+                visited[node] = true;
+
+                for (int next = 0; next < isConnected.Length; next++)
+                {
+                    if(isConnected[node][next] == 1 && !visited[next])
+                    {
+                        DFS(isConnected, visited, next);
+                    }                   
+                }
+            }
+        }
+    }
+}
