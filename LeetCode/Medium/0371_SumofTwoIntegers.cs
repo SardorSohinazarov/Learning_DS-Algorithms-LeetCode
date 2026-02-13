39f29c17078b3ad6e33e766c53667bd54d1b4bad
Sohinazarov Sardorbek O'rinboy o'gli <Sohinazarov_S@dt.local>
2026-02-13 15:58:55 +0500

    get sum

---
+++ LeetCode/Medium/0371_SumofTwoIntegers.cs
@@ -0,0 +1,35 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace LeetCode.Medium
+{
+    public class _0371_SumofTwoIntegers
+    {
+        public class Solution
+        {
+            //private static void Main()
+            //{
+            //    var solution = new Solution();
+            //    int a = 1, b = 2;
+            //    var result = solution.GetSum(a, b);
+            //    Console.WriteLine(result); // Output: 3
+            //}
+
+            public int GetSum(int a, int b)
+            {
+                int carry = 0;
+                int result = 0;
+
+                for (int i = 0; i < 32; i++)
+                {
+                    int bitA = (a >> i) & 1;
+                    int bitB = (b >> i) & 1;
+                    int sum = bitA ^ bitB ^ carry;
+                    carry = (bitA & bitB) | (carry & (bitA ^ bitB));
+                    result |= (sum << i);
+                }
+
+                return result;
+            }
+        }
+    }
+}
